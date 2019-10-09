a5f6ddb5272f4c5a2cd7cb95f37eec36b7e9022d
Gatien GNAKOU-EDJAMBO <gatiengnakou@gmail.com>
2019-10-09 08:33:04 +0000

    Add Remaining Calculations
    Calculer les interets totaux et les retraits totaux

--- script.js
+++ script.js
@@ -21,23 +21,25 @@
             let investissement, duree, retrait, retirer, pourcentage;
             investissement = document.querySelector("#montant").value;
             duree = document.querySelector("#duree").value;
-            retrait = document.querySelector("#retrait").value;
+            retrait = +document.querySelector("#retrait").value;
             retirer = document.querySelector("#retirer").value;
             pourcentage = document.querySelector("#pourcentage").value;
             fieldsOk(document.querySelectorAll("input"));
 
             let solde = 0;
             let totalRetrait = 0;
-
+            let totalInterets=0;
             computeFields();
             function computeFields() {
                 solde = +investissement;
                 console.log('computing')
                 for (let i = 0; i < duree; i++) {
                     solde += investissement * pourcentage / 100;
+                    totalInterets+=investissement*pourcentage/100;
                     if (retirer) {
                         solde -= retrait;
                         totalRetrait += retrait;
+                        totalInterets-=retrait;
                     }
                     investissement = solde;
                 }
@@ -45,6 +47,8 @@
             }
             function updateUi() {
                 document.querySelector("#solde_compte").textContent = "Solde: " + solde.toLocaleString("fr-FR", { style: "currency", currency: "XOF" });
+                document.querySelector("#total_retrait").textContent = " Total retrait: " + totalRetrait.toLocaleString("fr-FR", { style: "currency", currency: "XOF" });
+                document.querySelector("#interets").textContent = " Total Interets: " + totalInterets.toLocaleString("fr-FR", { style: "currency", currency: "XOF" });
             }
             function fieldsOk(fields) {
                 fields.forEach(field => {
